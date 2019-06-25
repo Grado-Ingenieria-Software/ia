217eb826268aa7c0f73e1f4639c1f419e4cf0631
jmsx <josgongut1@alum.us.es>
2019-06-25 19:14:40 +0200

    Update

--- src/inicio.py
+++ src/inicio.py
@@ -7,13 +7,16 @@ from pomdp_runner import PomdpRunner
 from util import RunnerParams
 
 #Elegir el Problema
-opciones = ["Tigre", "Tiger-2D","Tiger-3D", "TigerProblemData","TAG", "Opcional 1", "Opcional 2"]
+opciones = ["Tigre", "TAG", "Laberinto", "Indiana Jones"]
 imprimeMenu(opciones)
+opcionesNormalizadas = ["Tigre", "TAG", "Laberinto", "IndianaJones"]
 res = input("Elige un problema: ")
-problema = opciones[int(res)-1]
+problema = opcionesNormalizadas[int(res)-1]
 
 #Elegir el algoritmo
 opciones = ["POMCP", "PBVI"]
+if problema == "TAG":
+    opciones = ["POMCP"]
 opcionesNormalizadas = ["pomcp", "pbvi"]
 imprimeMenu(opciones)
 res = input("Elige un algoritmo : ")
@@ -27,7 +30,7 @@ res = input("Elige un modo de simulación : ")
 modo = opcionesNormalizadas[int(res)-1]
 
 
-    # Ejecuta el problema
+# Ejecuta el problema
 parser = argparse.ArgumentParser(description='Solve pomdp')
 parser.add_argument('--config', type=str, default=algoritmo, help='The file name of algorithm configuration (without JSON extension)')
 parser.add_argument('--env', type=str, default=problema+'.POMDP', help='The name of environment\'s config file')
@@ -35,14 +38,18 @@ parser.add_argument('--budget', type=float, default=float('inf'), help='The tota
 parser.add_argument('--snapshot', type=bool, default=False, help='Whether to snapshot the belief tree after each episode')
 parser.add_argument('--logfile', type=str, default=None, help='Logfile path')
 parser.add_argument('--random_prior', type=bool, default=False, help='Whether or not to use a randomly generated distribution as prior belief, default to False')
-parser.add_argument('--max_play', type=int, default=10, help='Maximum number of play steps')
+parser.add_argument('--max_play', type=int, default=100, help='Maximum number of play steps')
 
 args = vars(parser.parse_args())
 params = RunnerParams(**args)
 
+if modo == "benchmark":
+    numeroEjecuciones = 30
+else:
+    numeroEjecuciones = 1
+
+
 with open(params.algo_config) as algo_config:
     algo_params = json.load(algo_config)
     runner = PomdpRunner(params)
-    runner.run(modo, **algo_params)
-#else:
-    #erfew
+    runner.run(numeroEjecuciones,modo, **algo_params)
--- src/pomdp_runner.py
+++ src/pomdp_runner.py
@@ -1,5 +1,6 @@
 import os
 
+from funcionesEstadistica import *
 from models import RockSampleModel, Model
 from solvers import POMCP, PBVI
 from parsers import PomdpParser, GraphViz
@@ -40,68 +41,98 @@ class PomdpRunner:
         visualiser.update(tree.root)
         visualiser.render('./dev/snapshots/{}'.format(filename))  # TODO: parametrise the dev folder path
 
-    def run(self, modo, algo, T, **kwargs):
-        visualiser = GraphViz(description='tmp')
-        params, pomdp = self.params, None
-        total_rewards, budget = 0, params.budget
+    def run(self, numeroEjecuciones,modo, algo, T, **kwargs):
+        pasos = []
+        recompensas = []
+        while numeroEjecuciones != 0:
+            params, pomdp = self.params, None
+            total_rewards, budget = 0, params.budget
+            if modo != "benchmark":
+                log.info('~~~ Inicializando ~~~')
+            with PomdpParser(params.env_config) as ctx:
+                # creates model and solver
+                model = self.create_model(ctx.copy_env())
+                pomdp = self.create_solver(algo, model)
 
-        log.info('~~~ initialising ~~~')
-        with PomdpParser(params.env_config) as ctx:
-            # creates model and solver
-            model = self.create_model(ctx.copy_env())
-            pomdp = self.create_solver(algo, model)
+                # supply additional algo params
+                belief = ctx.random_beliefs() if params.random_prior else ctx.generate_beliefs()
 
-            # supply additional algo params
-            belief = ctx.random_beliefs() if params.random_prior else ctx.generate_beliefs()
+                if algo == 'pbvi':
+                    belief_points = ctx.generate_belief_points(kwargs['stepsize'])
+                    pomdp.add_configs(belief_points)
+                elif algo == 'pomcp':
+                    pomdp.add_configs(budget, belief, **kwargs)
 
-            if algo == 'pbvi':
-                belief_points = ctx.generate_belief_points(kwargs['stepsize'])
-                pomdp.add_configs(belief_points)
-            elif algo == 'pomcp':
-                pomdp.add_configs(budget, belief, **kwargs)
+            # have fun!
+            if modo != "benchmark":
+                log.info('''
+                ++++++++++++++++++++++
+                    Estado inicial:  {}
+                    Presupuesto inicial:  {}
+                    Creencia inicial: {}
+                    Horizonte temporal: {}
+                    Max Play: {}
+                    Modo: {}
+                ++++++++++++++++++++++'''.format(model.curr_state, budget, belief, T, params.max_play, modo))
 
-        # have fun!
-        log.info('''
-        ++++++++++++++++++++++
-            Starting State:  {}
-            Starting Budget:  {}
-            Init Belief: {}
-            Time Horizon: {}
-            Max Play: {}
-            Modo: {}
-        ++++++++++++++++++++++'''.format(model.curr_state, budget, belief, T, params.max_play, modo))
+            condicionParada = False
+            i = 0
+            while not condicionParada:
+                # plan, take action and receive environment feedbacks
+                pomdp.solve(modo, T)
+                action = pomdp.get_action(belief)
+                new_state, obs, reward, cost = pomdp.take_action(action)
+                
+                # update states
+                belief = pomdp.update_belief(belief, action, obs)
+                total_rewards += reward
+                budget -= cost
 
-        for i in range(params.max_play):
-            # plan, take action and receive environment feedbacks
-            pomdp.solve(modo, T)
-            action = pomdp.get_action(belief)
-            new_state, obs, reward, cost = pomdp.take_action(action)
+                # print info
+                if modo == "iterativa":
+                    log.info('\n'.join([
+                    'Accion tomada: {}'.format(action),
+                    'Observacion: {}'.format(obs),
+                    'Recompensa: {}'.format(reward),
+                    'Presupuesto: {}'.format(budget),
+                    'Nuevo estado: {}'.format(new_state),
+                    'Nueva Creencia: {}'.format(belief),
+                    'Prueba numero: {}'.format(i),
+                    '=' * 20
+                    ]))
 
-            if params.snapshot and isinstance(pomdp, POMCP):
-                # takes snapshot of belief tree before it gets updated
-                self.snapshot_tree(visualiser, pomdp.tree, '{}.gv'.format(i))
-            
-            # update states
-            belief = pomdp.update_belief(belief, action, obs)
-            total_rewards += reward
-            budget -= cost
+                    if budget <= 0:
+                        log.info('Presupuesto superado.')
+                
+                if params.env == "Tigre.POMDP":
+                    condicionParada = action == "open-left" or action == "open-right" or i >= params.max_play
+                elif params.env == "TAG.POMDP":
+                    condicionParada = action == "Catch"  or i >= params.max_play
+                else:
+                    condicionParada = i >= params.max_play
+                
 
-            # print info
-            if modo == "iterativa":
-                log.info('\n'.join([
-                'Taking action: {}'.format(action),
-                'Observation: {}'.format(obs),
-                'Reward: {}'.format(reward),
-                'Budget: {}'.format(budget),
-                'New state: {}'.format(new_state),
-                'New Belief: {}'.format(belief),
-                'Prueba numero: {}'.format(i),
-                '=' * 20
-                ]))
+                i += 1
 
-                if budget <= 0:
-                    log.info('Budget spent.')
-
-            
-        log.info('{} games played. Total reward = {}'.format(i + 1, total_rewards))
+            if modo != "benchmark":   
+                log.info('{} pasos. Recompensa Total = {}'.format(i, total_rewards))
+            else:
+                log.info('{} | {} pasos. Recompensa Total = {}'.format(numeroEjecuciones, i, total_rewards))
+            pasos.append(i)
+            recompensas.append(total_rewards)
+            numeroEjecuciones -= 1
+        if modo == "benchmark":
+            log.info('\n'.join([
+            '#' * 20,
+            'Media de los pasos: {}'.format(media(pasos)),
+            'Varianza de los pasos: {}'.format(varianza(pasos)),
+            'Desviacion Tipida de los pasos: {}'.format(desviacion_tipica(pasos)),
+            '#' * 20
+            ]))
+            log.info('\n'.join([
+            'Media de la recompensa: {}'.format(media(recompensas)),
+            'Varianza de la recompensa: {}'.format(varianza(recompensas)),
+            'Desviacion Tipida de la recompensa: {}'.format(desviacion_tipica(recompensas)),
+            '#' * 20
+            ]))
         return pomdp
